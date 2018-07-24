63a419b2479b8b73258369296ed05db88a0decd0
kboutarel <15114753+kboutarel@users.noreply.github.com>
2018-07-24 00:08:54 -0400

    Completed all of the control packet structs

--- packet.go
+++ packet.go
@@ -5,7 +5,11 @@ package gomqtt
 type ControlPkt interface {
     // Marshall transforms the struct representation into bytes
     // to be sent over the wire.
-    Marshall() ([]byte, err)
+    Marshall() ([]byte, error)
+
+    // Unmarshall transforms the bytes received over the wire into
+    // the struct representation of the packet.
+    Unmarshall([]byte) (error)
 }
 
 // FixedHeader is the fixed header part of each control packet.
@@ -23,8 +27,7 @@ type FixedHeader struct {
 // CONNECT is a control packet used by clients to request a
 // connection to a server.
 //
-// Along with the fixed header, the CONNECT packet also contains the following
-// as part of its variable header:
+// The CONNECT packet contains the following as part of its variable header:
 //  - protocol name: "MQTT"
 //  - protocol level: 4
 //  - connect flags:
@@ -42,20 +45,15 @@ type FixedHeader struct {
 // connect flags, then a Will Topic and a Will Message are provided next in
 // the payload. Finally, a Username and/or Password will be in the payload
 // according to the User Name flag and Password flag in the connect flags,
-// respectively.
+// respectively. Therefore, only the Client Identifier is required in the
+// payload, the other values are optional.
 //
 // Defined in Section 3.1 of the specification.
 type CONNECT struct {
   FixedHeader
   Name string
   Level int
-  Reserved bool
-  CleanSession bool
-  WillFlag bool
-  WillQoS bool
-  WillRetain bool
-  PasswordFlag bool
-  UsernameFlag bool
+  CFlags CONNECTFlags
   ClientID string
   WillTopic string
   WillMessage string
@@ -63,7 +61,20 @@ type CONNECT struct {
   Password string
 }
 
-// CONNACK is a control packet to acknowledge connection requests.
+// CONNECTFlags represents the connect flags in the variable header of the
+// CONNECT control packet.
+//
+// Defined in Section 3.1.2.3 of the specification.
+type CONNECTFlags struct {
+  CleanSession bool
+  WillFlag bool
+  WillQoS int
+  WillRetain bool
+  PasswordFlag bool
+  UserNameFlag bool
+}
+
+// CONNACK is a response control packet to acknowledge connection requests.
 //
 // There is currently only a single flag being used, the Session Present Flag.
 // This tells a client if a session already existed on this server.
@@ -105,7 +116,213 @@ type CONNACK struct {
   ReturnCode int
 }
 
-// Unmarshall transforms bytes received from a connection into a control packet.
-func Unmarshall(b []byte) (pkt ControlPkt, err error) {
+// PUBLISH is a control packet to publish messages.
+//
+// The flags in the fixed header are:
+//  - DUP: whether this is a duplicate of a message that
+//    the sending party has already sent.
+//  - QoS level: level of assurance for delivery of the application message.
+//  - RETAIN: whether the application message in this packet should be retained
+//    so that future subscriptions receive this message.
+// The QoS level is defined according to this table:
+//  |-----------|-------|-------|----------------------------------------------|
+//  | QoS value | Bit 2 | Bit 1 | Description                                  |
+//  |-----------|-------|-------|----------------------------------------------|
+//  |     0     |   0   |   0   | At most once delivery                        |
+//  |-----------|-------|-------|----------------------------------------------|
+//  |     1     |   0   |   1   | At least once delivery                       |
+//  |-----------|-------|-------|----------------------------------------------|
+//  |     2     |   1   |   0   | Exactly once delivery                        |
+//  |-----------|-------|-------|----------------------------------------------|
+//  |     -     |   1   |   1   | Reserved - must not be used                  |
+//  |-----------|-------|-------|----------------------------------------------|
+//
+// The variable header of the PUBLISH packet contains a Topic Name as the
+// destination for this application message and a Packet Identifier to identify
+// this packet and acknowledgments for this packet.
+//
+// The payload contains the application message.
+//
+// Defined in Section 3.3 of the specification.
+type PUBLISH struct {
+  FixedHeader
+  DUP bool
+  QoS int
+  RETAIN bool
+  TopicName string
+  PktID uint16
+}
+
+// PUBACK is a response control packet to acknowledge a PUBLISH packet
+// with QoS level 1.
+//
+// The variable header of the PUBACK packet contains only a Packet Identifier
+// taken from the PUBLISH packet being acknowledged.
+//
+// The PUBACK packet has no payload.
+//
+// Defined in Section 3.4 of the specification.
+type PUBACK struct {
+  FixedHeader
+  PktID uint16
+}
+
+// PUBREC is a response control packet to a PUBLISH packet with QoS level 2.
+// This is the 2nd packet of the QoS 2 protocol exchange.
+//
+// The variable header of the PUBREC packet contains only a Packet Identifier
+// taken from the PUBLISH packet being acknowledged.
+//
+// The PUBREC packet has no payload.
+//
+// Defined in Section 3.5 of the specification.
+type PUBREC struct {
+  FixedHeader
+  PktID uint16
+}
+
+// PUBREL is a response control packet to a PUBREC packet.
+// This is the 3rd packet of the QoS 2 protocol exchange.
+//
+// The variable header of the PUBREL packet contains only a Packet Identifier
+// taken from the PUBREC packet being acknowledged.
+//
+// The PUBREL packet has no payload.
+//
+// Defined in Section 3.6 of the specification.
+type PUBREL struct {
+  FixedHeader
+  PktID uint16
+}
+
+// PUBCOMP is a response control packet to a PUBREL packet.
+// This is the 4th and final packet of the QoS 2 protocol exchange.
+//
+// The variable header of the PUBCOMP packet contains only a Packet Identifier
+// taken from the PUBCOMP packet being acknowledged.
+//
+// The PUBCOMP packet has no payload.
+//
+// Defined in Section 3.7 of the specification.
+type PUBCOMP struct {
+  FixedHeader
+  PktID uint16
+}
+
+// TopicQosPair is a pair of Topic Filter and QoS level contained in the payload
+// of a SUBSCRIBE control packet.
+//
+// Defined in Section 3.8.3 of the specification.
+type TopicQoSPair struct {
+  TopicFilter string
+  QoS int
+}
+
+// SUBSCRIBE is a control packet to subscribe to topics.
+//
+// The variable header of the SUBSCRIBE packet contains only a
+// Packet Identifier.
+//
+// The payload contains a list of Topic Filters matching a Topic Name paired
+// with a requested QoS level that the client wishes to subscribe to.
+//
+// Defined in Section 3.8 of the specification.
+type SUBSCRIBE struct {
+  FixedHeader
+  PktID uint16
+  TQPairs []TopicQoSPair
+}
+
+// SUBACK is a response control packet to acknowledge a SUBSCRIBE packet.
+//
+// The variable header of a SUBACK packet contains only a Packet Identifier
+// taken from the SUBSCRIBE packet being acknowledged.
+//
+// The payload contains a list of return codes for each Topic Filter/QoS pair
+// from the corresponding SUBSCRIBE packet.
+// The return code values are according to this table:
+//  |------|---------|------------------------|
+//  | Code | Result  | Description            |
+//  |------|---------|------------------------|
+//  | 0x00 | Success | Maximum QoS 0          |
+//  |------|---------|------------------------|
+//  | 0x01 | Success | Maximum QoS 1          |
+//  |------|---------|------------------------|
+//  | 0x02 | Sucess  | Maximum QoS 2          |
+//  |------|---------|------------------------|
+//  | 0x80 | Failure |                        |
+//  |------|---------|------------------------|
+//
+// Defined in Section 3.9 of the specification.
+type SUBACK struct {
+    FixedHeader
+    PktID uint16
+    ReturnCodes []int
+}
+
+// UNSUBSCRIBE is a control packet used to unsubscribe from topics.
+//
+// The variable header of the UNSUBSCRIBE packet contains only a
+// Packet Identifier.
+//
+// The payload contains a list of Topic Filters that the client wishes to
+// unsubscribe from.
+//
+// Defined in Section 3.10 of the specification.
+type UNSUBSCRIBE struct {
+  FixedHeader
+  PktID uint16
+  TopicFilters []string
+}
+
+// UNSUBACK is a response control packet to acknowledge an UNSUBSCRIBE packet.
+//
+// The variable header of a UNSUBACK packet contains only a Packet Identifier
+// taken from the UNSUBSCRIBE packet being acknowledged.
+//
+// The UNSUBACK packet has no payload.
+//
+// Defined in Section 3.11 of the specification.
+type UNSUBACK struct {
+  FixedHeader
+  PktID uint16
+}
+
+// PINGREQ is a control packet used for a PING request.
+//
+// The PINGREQ packet has no variable header.
+//
+// The PINGREQ packet has no payload.
+//
+// Defined in Section 3.12 of the specification.
+type PINGREQ struct {
+  FixedHeader
+}
+
+// PINGRESP is a response control packet used for a PING response.
+//
+// The PINGRESP packet has no variable header.
+//
+// The PINGRESP packet has no payload.
+//
+// Defined in Section 3.13 of the specification.
+type PINGRESP struct {
+  FixedHeader
+}
+
+// DISCONNECT is a control packet to notify of a client disconnection.
+//
+// The DISCONNECT packet has no variable header.
+//
+// The DISCONNECT packet has no payload.
+//
+// Defined in Section 3.14 of the specification.
+type DISCONNECT struct {
+  FixedHeader
+}
+
+// Unmarshall is a general function which transforms bytes into a ControlPkt
+// according to the MQTT Control Packet type extracted from the first byte.
+func Unmarshall(b []byte) (ControlPkt, error) {
   return nil, "NOT IMPLEMENTED"
 }
